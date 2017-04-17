a5ffaacf99e5d5fc8a9904d3bffe1e1fbf7c941c
guotianyou <fortianyou@gmail.com>
2017-04-17 19:52:02 +0800

    基本章节内容

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#encoding=utf-8
+#########################################################################
+# File Name: compile.sh
+# Author: GuoTianyou
+# mail: fortianyou@gmail.com
+# Created Time: 五  4/ 7 10:52:46 2017
+#! /bin/bash
+
+#########################################################################
+latex thesis
+bibtext thesis
+latex thesis
+xelatex thesis
